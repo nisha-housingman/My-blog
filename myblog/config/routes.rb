5b0c7ddaa9a85c4263cf3b97b8cc6f95cf435ccc
Nisha <nisha@housingman.com>
2015-11-16 00:59:14 +0530

    Add pages

--- myblog/config/routes.rb
+++ myblog/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'posts#index'
-
+  get '/about', to: 'pages#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
